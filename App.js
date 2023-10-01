6251094ce7ab7f5bb3dd8177ea207eaf0ed1e3d4
Rakesh kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2023-10-01 09:17:50 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+
+
+
 const parent = React.createElement("div",
 {id:"parent"},[
     React.createElement("div",{id:"child"},
 [
-    React.createElement("h1",{ }, " I am h1 tag"),
-    React.createElement("h2",{ }, " I am h1 tag")
+    React.createElement("h1",{ }, " I am Rakesh  tag"),
+    React.createElement("h2",{ }, " I am Engineering Student tag")
 ]
 ),
 React.createElement("div",{id:"child2"},
@@ -19,5 +26,7 @@ React.createElement("div",{id:"child2"},
 
 const  heading =  React.createElement("h1", {id:"heading"}, "Hello World From React!");
 console.log(parent); //object
+
+
          const root= ReactDOM.createRoot(document.getElementById("root"));
          root.render(parent);
